c7054a752c5a6ea11aa9f53dce3c55f1e8247d62
Carlos corpus susanibar <xroberx01@hotmail.com>
2026-05-09 12:28:05 -0500

    feat: se protegio la ruta del admin

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware(['auth', 'es_admin'])->group(function () {
+    Route::get('/admin/dashboard', function () {
+        return "Bienvenido, Administrador CARLOS CORPUS";
+    });
 });
